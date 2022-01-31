1bc8b712c6958e179e212467d46ed028146136bb
marijam25 <marijamilicevicc25@gmail.com>
2022-01-31 15:53:02 +0100

    Home komponenta napravljena, svi studenti ubaceni u niz, rutiranje preko react-router-dom biblioteke

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+
+function Home() {
+
+    var prijavljeniStudenti = [
+        { student: 'Marija Markovic', brojIndeksa: '12/18', domaci: 'Treci', asistent: 'Tamara Naumovic' },
+        { ime: 'Jovana Petrovic', brojIndeksa: '141/18', domaci: 'Drugi', asistent: 'Aleksa Miletic' },
+        { ime: 'Ivan Nikolic', brojIndeksa: '332/18', domaci: 'Prvi', asistent: 'Petar Lukovac' },
+        { ime: 'Dragana Jovanovic', brojIndeksa: '63/18', domaci: 'Prvi i Treci', asistent: 'Aleksa Miletic' },
+        { ime: 'Stefan Dragovic', brojIndeksa: '282/18', domaci: 'Prvi i Drugi', asistent: 'Petar Lukovac' },
+        { ime: 'Ana Milenkovic', brojIndeksa: '771/18', domaci: 'Prvi, Drugi i Treci', asistent: 'Tamara Naumovic' },
+        { ime: 'Filip Bojanic', brojIndeksa: '2/18', domaci: 'Drugi', asistent: 'Petar Lukovac' },
+        { ime: 'Nevena Stefanovic', brojIndeksa: '194/18', domaci: 'Prvi', asistent: 'Aleksa Miletic' },
+    ];
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Home
